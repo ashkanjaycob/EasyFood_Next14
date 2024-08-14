191f69981ffb3e0250260cece6210e39bba705cc
Ashkan Yaghobi <ashkan.yaghobii@gmail.com>
2024-08-15 00:39:49 +0330

    Updated Categories

---
+++ src/app/categories/page.js
@@ -0,0 +1,11 @@
+import CategoriesPage from "@/components/Modules/CategoriesPage"
+
+function categories() {
+  return (
+    <div>
+      <CategoriesPage />
+    </div>
+  )
+}
+
+export default categories
---
+++ src/components/Modules/CategoriesPage.js
@@ -0,0 +1,13 @@
+
+import styles from "./CategoriesPage.module.css";
+
+function CategoriesPage() {
+ 
+  return (
+    <div className={styles.container}>
+      <h2>Categories</h2>
+    </div>
+  );
+}
+
+export default CategoriesPage;
